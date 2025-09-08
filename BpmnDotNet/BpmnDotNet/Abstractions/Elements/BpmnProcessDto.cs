044815d4a575c5308c162195dff007e7063c895f
alex <monteristo1946@gmail.cim>
2025-09-08 09:05:56 +0300

    BpmnDotNet рефакторинг.

--- BpmnDotNet/BpmnDotNet/Abstractions/Elements/BpmnProcessDto.cs
+++ BpmnDotNet/BpmnDotNet/Abstractions/Elements/BpmnProcessDto.cs
@@ -1,8 +1,19 @@
 namespace BpmnDotNet.Abstractions.Elements;
 
-public class BpmnProcessDto(string idBpmnProcess, IElement[] elementsFromBody)
+/// <summary>
+/// Дто описывающая в объектном виде  структуру bpmn документа.
+/// </summary>
+/// <param name="idBpmnProcess">idBpmnProcess.</param>
+/// <param name="elementsFromBody">Элементы схемы.</param>
+internal class BpmnProcessDto(string idBpmnProcess, IElement[] elementsFromBody)
 {
+    /// <summary>
+    /// Gets iD процесса.
+    /// </summary>
     public string IdBpmnProcess { get; init; } = idBpmnProcess;
 
+    /// <summary>
+    /// Gets блоки bpmn схемы.
+    /// </summary>
     public IElement[] ElementsFromBody { get; init; } = elementsFromBody;
 }
--- BpmnDotNet/BpmnDotNet/Abstractions/Elements/Constants.cs
+++ BpmnDotNet/BpmnDotNet/Abstractions/Elements/Constants.cs
@@ -1,48 +1,152 @@
 namespace BpmnDotNet.Abstractions.Elements;
 
+/// <summary>
+/// Константы.
+/// </summary>
 internal static class Constants
 {
+    /// <summary>
+    /// bpmn:definitions.
+    /// </summary>
     public const string BpmnRootName = "bpmn:definitions";
+
+    /// <summary>
+    /// bpmn:process.
+    /// </summary>
     public const string BpmnProcessName = "bpmn:process";
+
+    /// <summary>
+    /// bpmn:startEvent.
+    /// </summary>
     public const string BpmnStartEventName = "bpmn:startEvent";
+
+    /// <summary>
+    /// bpmn:endEvent.
+    /// </summary>
     public const string BpmnEndEventName = "bpmn:endEvent";
 
+    /// <summary>
+    /// bpmn:outgoing.
+    /// </summary>
     public const string BpmnOutGoingName = "bpmn:outgoing";
+
+    /// <summary>
+    /// bpmn:incoming.
+    /// </summary>
     public const string BpmnIncomingName = "bpmn:incoming";
 
+    /// <summary>
+    /// bpmn:sequenceFlow.
+    /// </summary>
     public const string BpmnSequenceFlowName = "bpmn:sequenceFlow";
 
+    /// <summary>
+    /// bpmn:exclusiveGateway.
+    /// </summary>
     public const string BpmnExclusiveGatewayName = "bpmn:exclusiveGateway";
+
+    /// <summary>
+    /// bpmn:parallelGateway.
+    /// </summary>
     public const string BpmnParallelGatewayName = "bpmn:parallelGateway";
 
+    /// <summary>
+    /// bpmn:sendTask.
+    /// </summary>
     public const string BpmnSendTaskName = "bpmn:sendTask";
+
+    /// <summary>
+    /// bpmn:receiveTask.
+    /// </summary>
     public const string BpmnReceiveTaskName = "bpmn:receiveTask";
+
+    /// <summary>
+    /// bpmn:serviceTask.
+    /// </summary>
     public const string BpmnServiceTaskName = "bpmn:serviceTask";
 
+    /// <summary>
+    /// bpmn:subProcess.
+    /// </summary>
     public const string BpmnSubProcess = "bpmn:subProcess";
 
+    /// <summary>
+    /// targetRef.
+    /// </summary>
     public const string BpmnTargetRef = "targetRef";
+
+    /// <summary>
+    /// id.
+    /// </summary>
     public const string BpmnIdName = "id";
+
+    /// <summary>
+    /// sourceRef.
+    /// </summary>
     public const string BpmnSourceRef = "sourceRef";
 
-    //Drawing Schema
+    /// <summary>
+    /// bpmndi:BPMNDiagram.
+    /// </summary>
     public const string BpmnDiagramName = "bpmndi:BPMNDiagram";
+
+    /// <summary>
+    /// bpmndi:BPMNPlane.
+    /// </summary>
     public const string BpmnBpmnPlaneName = "bpmndi:BPMNPlane";
 
+    /// <summary>
+    /// bpmnElement.
+    /// </summary>
     public const string BpmnElementName = "bpmnElement";
 
+    /// <summary>
+    /// bpmndi:BPMNShape.
+    /// </summary>
     public const string BpmnShapeName = "bpmndi:BPMNShape";
+
+    /// <summary>
+    /// bpmndi:BPMNEdge.
+    /// </summary>
     public const string BpmnEdgeName = "bpmndi:BPMNEdge";
 
+    /// <summary>
+    /// dc:Bounds.
+    /// </summary>
     public const string BpmnBoundsName = "dc:Bounds";
 
+    /// <summary>
+    /// x.
+    /// </summary>
     public const string BpmnXName = "x";
+
+    /// <summary>
+    /// y.
+    /// </summary>
     public const string BpmnYName = "y";
+
+    /// <summary>
+    /// width.
+    /// </summary>
     public const string BpmnWidthName = "width";
+
+    /// <summary>
+    /// height.
+    /// </summary>
     public const string BpmnHeightName = "height";
 
+    /// <summary>
+    /// bpmndi:BPMNLabel.
+    /// </summary>
     public const string BpmnLabelName = "bpmndi:BPMNLabel";
+
+    /// <summary>
+    /// di:waypoint.
+    /// </summary>
     public const string BpmnWaypointName = "di:waypoint";
 
+    /// <summary>
+    /// name.
+    /// </summary>
     public const string BpmnAttributesName = "name";
 }
