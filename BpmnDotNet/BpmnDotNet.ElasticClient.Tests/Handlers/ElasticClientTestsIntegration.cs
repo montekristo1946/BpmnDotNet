c9453b8199cd2e946f94960ff895db1c3cc4325b
alex <monteristo1946@gmail.cim>
2025-11-15 15:03:51 +0300

    #3 Починил тесты

--- BpmnDotNet/BpmnDotNet.ElasticClient.Tests/Handlers/ElasticClientTestsIntegration.cs
+++ BpmnDotNet/BpmnDotNet.ElasticClient.Tests/Handlers/ElasticClientTestsIntegration.cs
@@ -3,32 +3,30 @@ using AutoFixture;
 using BpmnDotNet.Common.Abstractions;
 using BpmnDotNet.Common.BPMNDiagram;
 using BpmnDotNet.Common.Dto;
-using BpmnDotNet.ElasticClient;
-using BpmnDotNet.ElasticClient.Handlers;
 using Microsoft.Extensions.Logging;
 
-namespace BpmnDotNetElasticClientTests.Handlers;
+namespace BpmnDotNet.ElasticClient.Tests.Handlers;
 
 public class ElasticClientTestsIntegration
 {
-    private readonly ElasticClient _elasticClient;
+    private readonly ElasticClient.Handlers.ElasticClient _elasticClient;
     private readonly ILoggerFactory loggerFactory;
     private readonly Fixture _fixture;
+
     private string[] _filtersProcessStatus =
     [
         nameof(ProcessStatus.None), nameof(ProcessStatus.Works), nameof(ProcessStatus.Completed),
         nameof(ProcessStatus.Error)
     ];
-    
+
     public ElasticClientTestsIntegration()
     {
         var config = new ElasticClientConfig { ConnectionString = "http://localhost:9200" };
         var logger = LoggerFactory.Create(builder => { builder.AddConsole(); }).CreateLogger<IElasticClient>();
-        _elasticClient = new ElasticClient(config, logger);
+        _elasticClient = new ElasticClient.Handlers.ElasticClient(config, logger);
         _fixture = new Fixture();
     }
-    
-  
+
 
     [Fact]
     public async Task SetDataAsync_cheksLoadPlaneInDatabase_True()
@@ -37,7 +35,7 @@ public class ElasticClientTestsIntegration
         var plane = _fixture.Build<BpmnPlane>().With(p => p.Name, name).Create();
 
         var res = await _elasticClient.SetDataAsync(plane);
-     
+
         Assert.True(res);
     }
 
@@ -57,68 +55,68 @@ public class ElasticClientTestsIntegration
         var plane = _fixture.Build<BpmnPlane>().With(p => p.IdBpmnProcess, dBpmnProcess).Create();
 
         var resSetDataAsync = await _elasticClient.SetDataAsync(plane);
-        
-        var res = await _elasticClient.GetDataFromIdAsync<BpmnPlane>(dBpmnProcess,[]);
-        
+
+        var res = await _elasticClient.GetDataFromIdAsync<BpmnPlane>(dBpmnProcess, []);
+
         Assert.True(resSetDataAsync);
         Assert.NotNull(res);
-        Assert.Equal(dBpmnProcess,res.IdBpmnProcess);
+        Assert.Equal(dBpmnProcess, res.IdBpmnProcess);
     }
-    
+
     [Fact]
     public async Task GetCountHistoryNodeStateAsync_GetCount_NotNull()
     {
         var dBpmnProcess = $"test_IdBpmnProcess";
         var historyNodeState = _fixture.Build<HistoryNodeState>()
             .With(p => p.IdBpmnProcess, dBpmnProcess)
-            .With(p=>p.DateCreated,DateTime.Now.Ticks)
-            .With(p=>p.DateLastModified,DateTime.Now.Ticks)
-            .With(p=>p.TokenProcess,DateTime.Now.Ticks.ToString)
+            .With(p => p.DateCreated, DateTime.Now.Ticks)
+            .With(p => p.DateLastModified, DateTime.Now.Ticks)
+            .With(p => p.TokenProcess, DateTime.Now.Ticks.ToString)
             .Create();
 
         var resSetDataAsync = await _elasticClient.SetDataAsync(historyNodeState);
-        
-        var res = await _elasticClient.GetCountHistoryNodeStateAsync(dBpmnProcess,_filtersProcessStatus,1);
-        
+
+        var res = await _elasticClient.GetCountHistoryNodeStateAsync(dBpmnProcess, _filtersProcessStatus, 1);
+
         Assert.True(resSetDataAsync);
-        Assert.Equal(1,res);
+        Assert.Equal(1, res);
     }
-    
-        
+
+
     [Fact]
     public async Task GetHistoryNodeStateAsync_GetHistory_NotNull()
     {
         var dBpmnProcess = $"test_IdBpmnProcess";
         var historyNodeState = _fixture.Build<HistoryNodeState>()
             .With(p => p.IdBpmnProcess, dBpmnProcess)
-            .With(p=>p.DateCreated,DateTime.Now.Ticks)
-            .With(p=>p.DateLastModified,DateTime.Now.Ticks)
-            .With(p=>p.TokenProcess,DateTime.Now.Ticks.ToString)
+            .With(p => p.DateCreated, DateTime.Now.Ticks)
+            .With(p => p.DateLastModified, DateTime.Now.Ticks)
+            .With(p => p.TokenProcess, DateTime.Now.Ticks.ToString)
             .Create();
 
         var resSetDataAsync = await _elasticClient.SetDataAsync(historyNodeState);
-        
-        var res = await _elasticClient.GetHistoryNodeStateAsync(dBpmnProcess,0,1,_filtersProcessStatus);
-        
+
+        var res = await _elasticClient.GetHistoryNodeStateAsync(dBpmnProcess, 0, 1, _filtersProcessStatus);
+
         Assert.True(resSetDataAsync);
         Assert.NotNull(res);
     }
-    
+
     [Fact]
     public async Task GetHistoryNodeFromTokenMaskAsync_GetHistory_NotNull()
     {
         var dBpmnProcess = $"test_IdBpmnProcess";
         var historyNodeState = _fixture.Build<HistoryNodeState>()
             .With(p => p.IdBpmnProcess, dBpmnProcess)
-            .With(p=>p.DateCreated,DateTime.Now.Ticks)
-            .With(p=>p.DateLastModified,DateTime.Now.Ticks)
-            .With(p=>p.TokenProcess,DateTime.Now.Ticks.ToString)
+            .With(p => p.DateCreated, DateTime.Now.Ticks)
+            .With(p => p.DateLastModified, DateTime.Now.Ticks)
+            .With(p => p.TokenProcess, DateTime.Now.Ticks.ToString)
             .Create();
 
         var resSetDataAsync = await _elasticClient.SetDataAsync(historyNodeState);
-        
-        var res = await _elasticClient.GetHistoryNodeFromTokenMaskAsync(dBpmnProcess,"*",1);
-        
+
+        var res = await _elasticClient.GetHistoryNodeFromTokenMaskAsync(dBpmnProcess, "*", 1);
+
         Assert.True(resSetDataAsync);
         Assert.NotNull(res);
     }
